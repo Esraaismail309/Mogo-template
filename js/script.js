9770bbb6f57e7c7b75c4c27f0f397072de59ecc2
Esraaismail309 <esraaismail309@gmail.com>
2021-10-27 18:30:23 +0200

    add fontawesome

--- js/script.js
+++ js/script.js
@@ -5,18 +5,29 @@
 
     let lang = document.getElementById("lang")
     let arabic = document.getElementById("arabic")
+    const changeLang = () => {
+        if (arabic.innerHTML == "AR") {
+            lang.setAttribute("dir", "rtl")
+            arabic.innerHTML = "EN"
+            $('link[href="./css/bootstrap.min.css"]').attr("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css")
+        } else
+        if (arabic.innerHTML == "EN") {
+            lang.setAttribute("dir", "ltr")
+            arabic.innerHTML = "AR"
+            $("href", "https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.rtl.min.css").attr('link[href="./css/bootstrap.min.css"]')
+        }
 
-    function changeLang() {
-        lang.setAttribute("dir", "rtl")
-        arabic.innerHTML("Eng")
     }
-    $(document).ready(function() {
-        $(window).scroll(function() {
-            if ($(window).scrollTop() > $(window).height()) {
-                $(".navbar").css({ "background-color": "#a3988f" });
-            } else {
-                $(".navbar").css({ "background-color": "transparent" });
-            }
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > $(window).height()) {
+            $(".navbar").css({ "background-color": "#a3988f" });
+        } else {
+            $(".navbar").css({ "background-color": "transparent" });
+        }
 
-        })
-    })
+    });
+    // $('a').click(function(e) {
+    //     $('html, body').animate({
+    //         scrollTop: $('#theBird').offset().top // Use element id to get element's location.
+    //     }, 4000);
+    // });
